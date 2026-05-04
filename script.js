3cfcca8bb16d83097a01af1e53bfba48005773ea
5udixAdmin <sudisht2802+github@gmail.com>
2026-05-04 20:49:23 +0530

    added files for the web hosting

---
+++ script.js
@@ -0,0 +1,8 @@
+function navigate(page) {
+    window.location.href = page;
+}
+
+function showMessage() {
+    document.getElementById("message").innerText =
+        "Thanks for visiting! More updates coming soon.";
+}
